cc73c6f9b7bdf5f6decb6e4383d2c89b37702b84
Aravinth Kaneshalingam <aravinth_kanesh@hotmail.com>
2026-04-03 18:07:47 +0100

    Add: clone-graph - submission-9

---
+++ Data Structures & Algorithms/clone-graph/submission-9.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        clones = {node: Node(node.val)}
+        queue = deque([node])
+
+        while queue:
+            curr = queue.popleft()
+
+            for neighbor in curr.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+
+                # Add neighbor to the CLONE, not original
+                clones[curr].neighbors.append(clones[neighbor])
+
+        return clones[node]
