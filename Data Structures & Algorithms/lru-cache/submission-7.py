5096d6589f022a77b836db6661b9d87c9f7d5e0e
Aravinth Kaneshalingam <aravinth_kanesh@hotmail.com>
2026-05-19 18:24:58 +0100

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self):
+        self.key, self.val = None, 0
+        self.prev, self.next = None, None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.mru, self.lru = Node(), Node()
+        self.lru.next, self.mru.prev = self.mru, self.lru
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+
+            self.remove(node)
+            self.insert(node)
+
+            return node.val
+
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+
+            self.remove(node)
+
+            node.val = value
+            self.insert(node)
+
+            return
+
+        node = Node()
+        node.key, node.val = key, value
+
+        self.cache[key] = node
+        self.insert(node)
+
+        if len(self.cache) > self.capacity:
+            lru_node = self.lru.next
+            self.remove(lru_node)
+            del self.cache[lru_node.key]
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev = self.mru.prev
+        prev.next, node.prev = node, prev
+        node.next, self.mru.prev = self.mru, node
+        
