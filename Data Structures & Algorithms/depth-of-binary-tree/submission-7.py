8e0e1a869972698badad163c63efc7f07a3712d5
Aravinth Kaneshalingam <aravinth_kanesh@hotmail.com>
2026-05-20 18:38:01 +0100

    Update: depth-of-binary-tree - submission-7

--- Data Structures & Algorithms/depth-of-binary-tree/submission-7.py
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-7.py
@@ -10,16 +10,5 @@ class Solution:
         if not root:
             return 0
 
-        stack = [(root, 1)]
-        max_depth = 0
-
-        while stack:
-            node, level = stack.pop()
-            max_depth = max(max_depth, level)
-
-            if node.left:
-                stack.append((node.left, level + 1))
-            if node.right:
-                stack.append((node.right, level + 1))
-
-        return max_depth
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
