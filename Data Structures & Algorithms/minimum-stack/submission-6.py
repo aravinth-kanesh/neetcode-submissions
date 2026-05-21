38ee52d91f8c2132b7fff615884281ad429e2a57
Aravinth Kaneshalingam <aravinth_kanesh@hotmail.com>
2026-05-21 19:09:32 +0100

    Update: minimum-stack - submission-6

--- Data Structures & Algorithms/minimum-stack/submission-6.py
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -1,19 +1,14 @@
 class MinStack:
     def __init__(self):
-        # stores (val, min_val) tuple
         self.stack = []
 
     def push(self, val: int) -> None:
-        if not self.stack:
-            self.stack.append((val, val))
-        else:
-            min_val = self.getMin()
-            self.stack.append((val, min(val, min_val)))
+        min_val = min(self.stack[-1][1], val) if self.stack else val
+        self.stack.append((val, min_val))
 
     def pop(self) -> None:
-        # don't need to check for empty stack edge-case
         self.stack.pop()
-
+        
     def top(self) -> int:
         return self.stack[-1][0]
 
